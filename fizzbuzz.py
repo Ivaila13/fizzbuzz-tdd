c020a969cc46849fb8790fe38513a9781fd50134
Ivaila13 <20113@uktc-bg.com>
2025-02-14 09:10:50 +0200

    Refactor FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,7 @@
 def fizzbuzz(n):
-    if n%3==0 and n % 5==0:
-      return "FizzBuzz"
+    result=""
     if n %3==0:
-       return "Fizz"
+       result +="Fizz"
     if n%5==0:
-       return "Buzz"
-    return n
+       result += "Buzz"
+    return result if result else n
